9227672c0f6508a195b4a89ca52e60c6ebb72078
Fredlina Devhania Kholishah <devhania88@gmail.com>
2024-09-04 15:43:08 +0700

    menambahkan footer dan javascript pada index.php

--- index.php
+++ index.php
@@ -100,5 +100,12 @@
             </div>
         </div>
     </div>
-    </body>
+    <footer class="footer text-white text-center py-3">
+        <p>&copy; Tugas Contact Form Pemwork. All rights reserved.</p>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
 </html>
