ae2c032d358c429eb70a7f9ab4e152790bed2650
Fredlina Devhania Kholishah <devhania88@gmail.com>
2024-09-04 15:50:32 +0700

    menambahkan body dalam home.php

--- home.php
+++ home.php
@@ -5,3 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - Contact Form Application</title>
+    <body>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="#">Contact Form</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8 text-center">
+                <div class="jumbotron">
+                    <img src="Fredlina.jpeg" alt="Profile" class="profile-img mb-4">
+                    <h1>Welcome Gaes,hehehehhehe!!!!</h1>
+                    <p class="lead">Hi im Fredlina Devhania Kholishah, kelas T3G</p>
+                    <hr class="my-4" style="border-top: 1px solid white;">
+                    <p>You can navigate to the Contact Us page to submit your information.</p>
+                    <a class="btn btn-light btn-lg" href="index.php" role="button">Go to Contact Us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
